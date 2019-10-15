da51c66cd04980b0380d1d2be647ef52c985af51
Andrew Doan <andrewdoancs@gmail.com>
2019-10-15 11:23:39 -0700

    cleanup

--- bestfaceforward/src/components/aboutus.js
+++ bestfaceforward/src/components/aboutus.js
@@ -10,27 +10,14 @@ export default class About extends Component {
     return (
       
       <div className="profiles">
-        <div className="andrew">
-        <Profile image="https://media.licdn.com/dms/image/C5603AQHaYrsskcDsfw/profile-displayphoto-shrink_200_200/0?e=1558569600&v=beta&t=PQHxO0Y2VmoPHK3HrKDgG3R0eodNuMCuG0oxmN3rALQ" 
+        <div className="profile">
+        <Profile image="https://i.ibb.co/Rv4NXCs/DOAN.png"
         name="Andrew Doan"
         yearMajor="UCSB 3rd Year Computer Engineer"
         linkedIn="https://www.linkedin.com/in/andrewadoan/"
         />
         </div>
-        <div className="benson">
-        <Profile image="https://media.licdn.com/dms/image/C5603AQHAufc89IlBMQ/profile-displayphoto-shrink_800_800/0?e=1556150400&v=beta&t=hDDhS5rTZ4ACCQW3Va-jv-WQrGzHkn7NiIsvqvVrbjc"
-        name="Jeffrey Li"
-        yearMajor="UCSB 3rd Year Electrical Engineer"
-        linkedIn="https://www.linkedin.com/in/jeffreyli01/"
-        />
-        </div>
-        <div className="tim">
-        <Profile image="https://media.licdn.com/dms/image/C5603AQErNhvSGTGsDQ/profile-displayphoto-shrink_800_800/0?e=1556150400&v=beta&t=DBwgLo_GOEpOZbSm3O0YvXpS6HAzw3wTwcNIC7RmWio"
-        name="Silas Collins"
-        yearMajor="UCSB 3rd Year Electrical Engineer"
-        linkedIn="https://www.linkedin.com/in/silas-collins-5b782016a/"
-        />
-        </div>
+        
       </div>
     )
   }
