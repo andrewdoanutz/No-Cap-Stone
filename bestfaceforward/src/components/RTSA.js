48ecf2fa8dd04ef836a39be9c4d6240bf00fee44
Ryan Gormley <rgormley@umail.ucsb.edu>
2019-10-16 15:57:49 -0700

    RG added universal sentence decoder to RTSA.js, need to figure out how to display it

--- bestfaceforward/src/components/RTSA.js
+++ bestfaceforward/src/components/RTSA.js
@@ -1,9 +1,28 @@
+//sentiment analysis
 import React, { Component } from 'react'
+import * as use from '@tensorflow-models/universal-sentence-encoder';
+
+// Load the model.
+var embeddings = use.load().then(model => {
+  // Embed an array of sentences.
+  const sentences = [
+    'Hello.',
+    'How are you?'
+  ];
+  model.embed(sentences).then(embeddings => {
+    // `embeddings` is a 2D tensor consisting of the 512-dimensional embeddings for each sentence.
+    // So in this example `embeddings` has the shape [2, 512].
+    embeddings.print(true /* verbose */);
+  });
+});
+
 
 export default class RTSA extends Component {
+
   render() {
     return (
       <div>
+
         <div>RTSA</div>
         <div>RTSA</div>
         <div>RTSA</div>
@@ -16,6 +35,7 @@ export default class RTSA extends Component {
         <div>RTSA</div>
         <div>RTSA</div>
         <div>RTSA</div>
+
       </div>
     )
   }
