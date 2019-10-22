a939dd4e2dd7c1142f11af8d9ed3b87e24ec2bb3
UCSB <ucsb@169-231-157-19.wireless.ucsb.edu>
2019-10-21 18:27:45 -0700

    Added framework before importn agora io for video call #7-Video_Cll

--- bestfaceforward/src/App.js
+++ bestfaceforward/src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route} from "react-router-dom";
 
-import NavBar from './components/navbar';
-import Login from './components/login.js';
+import NavBar from './components/navbar'
+import Login from './components/login.js'
 import About from './components/aboutus'
 import RTSA from './components/RTSA'
-import Video from './components/video'
+import Video from './components/Video'
 import S2TP from './components/S2TP'
 import S2TRT from './components/S2TRT'
 import Display from './components/display'
 import './css/App.css';
 
 class App extends Component {
-  
+
   render() {
     return (
       <div>
@@ -20,7 +20,7 @@ class App extends Component {
         <div>
           <NavBar/>
           <Route exact path="/RTSA" component={RTSA} />
-          <Route exact path="/video" component={Video} />
+          <Route exact path="/Video" component={Video} />
           <Route exact path="/S2TP" component={S2TP} />
           <Route exact path="/S2TRT" component={S2TRT} />
           <Route exact path="/display" component={Display} />
---
+++ bestfaceforward/src/components/Call.js
@@ -0,0 +1,8 @@
+
+import React, { Component } from "react";
+
+export default class Call extends Component {
+  render() {
+    return <div>Video Call</div>;
+  }
+}
