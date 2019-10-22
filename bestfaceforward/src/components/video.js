0310b918033e37f9c05f31fdc1a22d490cca19e7
UCSB <ucsb@169-231-157-19.wireless.ucsb.edu>
2019-10-21 18:27:45 -0700

    Added framework before importn agora io for video call #7-Video_Cll

--- bestfaceforward/src/components/video.js
+++ bestfaceforward/src/components/video.js
@@ -1,22 +1,22 @@
 import React, { Component } from 'react'
 
+import MeetingForm from './MeetingForm'
+import Call from './Call'
+
 export default class Video extends Component {
   render() {
     return (
       <div>
-        <div>Video</div>
-        <div>Video</div>
-        <div>Video</div>
-        <div>Video</div>
-        <div>Video</div>
-        <div>Video</div>
-        <div>Video</div>
-        <div>Video</div>
-        <div>Video</div>
-        <div>Video</div>
-        <div>Video</div>
-        <div>Video</div>
+        <div> TEST </div>
+        <div> TEST </div>
+        <div> TEST </div>
+        <div> TEST </div>
+        <div className="App">
+          <MeetingForm />
+          <Call />
+        </div>
       </div>
-    )
+
+    );
   }
 }
