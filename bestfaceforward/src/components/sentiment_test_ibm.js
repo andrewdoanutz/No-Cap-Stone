7e399e672bf8c0c82c0a17434d030ac746a54709
Adjon Tahiraj <atahiraj@umail.ucsb.edu>
2019-10-23 16:28:07 -0700

    IBM working with console input to test whatever is needed

--- bestfaceforward/src/components/sentiment_test_ibm.js
+++ bestfaceforward/src/components/sentiment_test_ibm.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+const readline = require('readline');
 const ToneAnalyzerV3 = require('ibm-watson/tone-analyzer/v3');
 const { IamAuthenticator } = require('ibm-watson/auth');
 
@@ -9,20 +11,24 @@ const toneAnalyzer = new ToneAnalyzerV3({
   url: 'https://gateway.watsonplatform.net/tone-analyzer/api',
 });
 
-const text = 'Team, I know that times are tough! Product '
-  + 'sales have been disappointing for the past three '
-  + 'quarters. We have a competitive product, but we '
-  + 'need to do a better job of selling it!';
-
-const toneParams = {
-  toneInput: { 'text': text },
-  contentType: 'application/json',
-};
-
-toneAnalyzer.tone(toneParams)
-  .then(toneAnalysis => {
-    console.log(JSON.stringify(toneAnalysis, null, 2));
-  })
-  .catch(err => {
-    console.log('error:', err);
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
   });
+
+rl.question('Please enter a short paragraph for Watson to analyze...', (text) => {
+    
+    const toneParams = {
+        toneInput: { 'text': text },
+        contentType: 'application/json',
+    };
+    toneAnalyzer.tone(toneParams)
+        .then(toneAnalysis => {
+            console.log(JSON.stringify(toneAnalysis, null, 2));
+        })
+        .catch(err => {
+            console.log('error:', err);
+        });
+    
+      rl.close();
+    });
