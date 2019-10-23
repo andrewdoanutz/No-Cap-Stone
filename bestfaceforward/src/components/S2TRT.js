89e1d216200668c48b16d2bd8d387e9a5b0b484c
Tim Chang <tinghaur@umail.ucsb.edu>
2019-10-22 18:13:03 -0700

    Got Google Translate API working

--- bestfaceforward/src/components/S2TRT.js
+++ bestfaceforward/src/components/S2TRT.js
@@ -1,7 +1,35 @@
 import React, { Component } from 'react'
+import SpeechRecognition from 'react-speech-recognition'
 
-export default class S2TRT extends Component {
+var ts = ""
+class S2TRT extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      transcript: "",
+      resetTranscript: "",
+      browserSupportsSpeechRecognition: false
+    };
+  }
+
+ translate(){
+  console.log(`guccidog`)
+  ts = "Adjon is a human dog"
+  var googleTranslate = require('google-translate')('AIzaSyB8yZoHinIGXbAXAo92QpQib10k93xgz_Y');
+  console.log(ts)
+  googleTranslate.translate(ts, 'ru', function(err, translation) {
+    console.log(translation);
+    // =>  { translatedText: 'Hallo', originalText: 'Hello', detectedSourceLanguage: 'en' }
+  });
+ }
   render() {
+
+    const { transcript, resetTranscript, browserSupportsSpeechRecognition } = this.props
+
+    if (!browserSupportsSpeechRecognition) {
+      return null
+    }
+    ts = transcript
     return (
       <div>
         <div>S2TRT</div>
@@ -15,8 +43,16 @@ export default class S2TRT extends Component {
         <div>S2TRT</div>
         <div>S2TRT</div>
         <div>S2TRT</div>
-        <div>S2TRT</div>
+        <div>
+          <button onClick={resetTranscript}>Reset</button>
+          <span>{transcript}</span>
+        </div>
+        <div>
+        <button onClick={this.translate}>Translate</button>
+        </div>
       </div>
     )
   }
 }
+
+export default SpeechRecognition(S2TRT)
