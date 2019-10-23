fc8c74521fca9cdbfd6a5bee2f0b73eb3d5d349b
Adjon Tahiraj <atahiraj@umail.ucsb.edu>
2019-10-23 15:57:40 -0700

    AT - IBM tone analyzer working in sentiment_test_ibm.js still need to put it on RTSA.js and test

---
+++ bestfaceforward/src/components/sentiment_test_ibm.js
@@ -0,0 +1,28 @@
+const ToneAnalyzerV3 = require('ibm-watson/tone-analyzer/v3');
+const { IamAuthenticator } = require('ibm-watson/auth');
+
+const toneAnalyzer = new ToneAnalyzerV3({
+  version: '2017-09-21',
+  authenticator: new IamAuthenticator({
+    apikey: 'ZpQzJCzwFyXMj9EYlLwMpiCbOf6qBmJ6tBGuYDjbJy9Q',
+}),
+  url: 'https://gateway.watsonplatform.net/tone-analyzer/api',
+});
+
+const text = 'Team, I know that times are tough! Product '
+  + 'sales have been disappointing for the past three '
+  + 'quarters. We have a competitive product, but we '
+  + 'need to do a better job of selling it!';
+
+const toneParams = {
+  toneInput: { 'text': text },
+  contentType: 'application/json',
+};
+
+toneAnalyzer.tone(toneParams)
+  .then(toneAnalysis => {
+    console.log(JSON.stringify(toneAnalysis, null, 2));
+  })
+  .catch(err => {
+    console.log('error:', err);
+  });
