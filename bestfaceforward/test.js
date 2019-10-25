ad83f52ccba7f2a2629af610f1846780e4febc77
Ryan Gormley <rgormley@umail.ucsb.edu>
2019-10-25 15:20:36 -0700

    RG demo

--- bestfaceforward/test.js
+++ bestfaceforward/test.js
@@ -1,29 +1,5 @@
 
- // Imports the Google Cloud client library
- const language = require('@google-cloud/language');
-
- // Instantiates a client
- const client = new language.LanguageServiceClient();
-
- // The text to analyze
- const text = 'I had a bad day today';
-
- const document = {
-   content: text,
-   type: 'PLAIN_TEXT',
- };
-
- // Detects the sentiment of the text
- client.analyzeSentiment({document: document})
-  .then(results =>{
-    const sentiment = results[0].documentSentiment;
-
-
-
- console.log(`Text: ${text}`);
- console.log(`Sentiment score: ${sentiment.score}`);
- console.log(`Sentiment magnitude: ${sentiment.magnitude}`);
-  })
-  .catch(err=>{
-    console.error('ERROR',err);
-  })
+ var mains = require('./watson');
+ var x = "hi";
+ x = mains.main();
+ console.log (x);
--- bestfaceforward/watson.js
+++ bestfaceforward/watson.js
@@ -1,34 +1,46 @@
-const ToneAnalyzerV3 = require('ibm-watson/tone-analyzer/v3');
-const { IamAuthenticator } = require('ibm-watson/auth');
 
-const toneAnalyzer = new ToneAnalyzerV3({
-  version: '2017-09-21',
-  authenticator: new IamAuthenticator({
-    apikey: 'M0o5HbUt2oLKXBvYA2hpNeioDKY9AhLxkydJIt98vYhm',
-  }),
-  url: 'https://gateway.watsonplatform.net/tone-analyzer/api',
-});
+exports.main = function(){
+  const ToneAnalyzerV3 = require('ibm-watson/tone-analyzer/v3');
+  const { IamAuthenticator } = require('ibm-watson/auth');
 
-const text = 'Team, I know that times are tough! Product '
-  + 'sales have been disappointing for the past three '
-  + 'quarters. We have a competitive product, but we '
-  + 'need to do a better job of selling it!';
+  const toneAnalyzer = new ToneAnalyzerV3({
+    version: '2017-09-21',
+    authenticator: new IamAuthenticator({
+      apikey: 'M0o5HbUt2oLKXBvYA2hpNeioDKY9AhLxkydJIt98vYhm',
+    }),
+    url: 'https://gateway.watsonplatform.net/tone-analyzer/api',
+  });
 
-const toneParams = {
-  toneInput: { 'text': text },
-  contentType: 'text/plain',
-  sentences: false,
-};
+  const text = 'Team, I know that times are tough! Product '
+    + 'sales have been disappointing for the past three '
+    + 'quarters. We have a competitive product, but we '
+    + 'need to do a better job of selling it!';
 
 
+console.log("text: ", text);
 
-const tone = toneAnalyzer.tone(toneParams)
-  .then(toneAnalysis=> {
-     return(JSON.stringify(toneAnalysis, null, 2));
+  const toneParams = {
+    toneInput: { 'text': text },
+    contentType: 'text/plain',
+    sentences: false,
+  };
 
-  })
-.catch(err => {
-  console.log('error:', err);
-});
 
-console.log(tone);
+
+  global.tone = "Tone";
+
+
+
+  toneAnalyzer.tone(toneParams)
+    .then(toneAnalysis => {
+      console.log(JSON.stringify(toneAnalysis, null, 2));
+    })
+    .catch(err => {
+      console.log('error:', err);
+    });
+
+
+//  console.log(global.tone);
+//  console.log("hello");
+//  return 0;
+}
