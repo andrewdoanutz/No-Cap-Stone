bad5b473c51bba57df2f50675caefe6e712e7c9e
Ryan Gormley <rgormley@umail.ucsb.edu>
2019-10-30 13:46:12 -0700

    RG have dbtest page rendering

---
+++ bestfaceforward/src/components/Database.js
@@ -0,0 +1,25 @@
+import React, {Component} from "react";
+
+import "../css/profile.css";
+
+class Database extends Component{
+  constructor(){
+    super()
+  }
+
+
+  render(){
+    return(
+      <div>
+        <div> database has been rendered</div>
+        <div> database has been rendered</div>
+        <div> database has been rendered</div>
+        <div> database has been rendered</div>
+        <div> database has been rendered</div>
+      </div>
+    )
+  }
+}
+
+
+export default Database
---
+++ bestfaceforward/src/pages/DBTest.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import Database from '../components/Database'
+
+
+import "../css/about.css";
+
+
+export default class DBTest extends Component{
+  render(){
+    return(
+      <div>
+        <div> TestArea </div>
+        <div> TestArea </div>
+        <div> TestArea </div>
+        <div> TestArea </div>
+        <div> TestArea </div>
+        <Database/>
+      </div>
+    )
+  }
+
+
+}
