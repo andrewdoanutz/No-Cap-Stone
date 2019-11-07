c4ba2c927edb10d8656e0bc2be6ea1481b188e21
Ryan Gormley <rgormley@umail.ucsb.edu>
2019-11-06 18:06:52 -0800

    RG Added function calls to add and delete user

--- bestfaceforward/src/components/Database.js
+++ bestfaceforward/src/components/Database.js
@@ -17,12 +17,13 @@ class Database extends Component{
       region: "us-west-1",
       //endpoint: "http://localhost:8001",
       endpoint: "https://dynamodb.us-west-1.amazonaws.com",
-
-      //get from google doc:
-      // accessKeyId :,
-      // secretAccessKey:
+      //get from google drive
+      //accessKeyId : 
+    //  secretAccessKey:
     });
     this.dynamodb = new AWS.DynamoDB();
+    this.docClient = new AWS.DynamoDB.DocumentClient();
+    this.table = 'Users'
   }
 
 
@@ -34,7 +35,7 @@ class Database extends Component{
         <div> database has been rendered</div>
         <div> database has been rendered</div>
         <div> database has been rendered</div>
-        <button onClick={this.createTable()}>createTable</button>
+        <button onClick={this.deleteUser("ryang")}></button>
       </div>
     )
   }
@@ -42,7 +43,7 @@ class Database extends Component{
 
   createTable(){
     var params = {
-      TableName : "Users3",
+      TableName : this.table,
       KeySchema: [
         //only put keys in here
         //Keytype Hash because they can be uniquely identified by username
@@ -72,9 +73,65 @@ class Database extends Component{
           )
       }
     });
-
   }
 
+  //can change the hardcoded variables
+  addUser(username, password, firstName, lastName, email){
+    //change this part to recieve input
+  //  var company = "UCSB";
+    var isInterviewer = true;
+    var isRecruiter = false;
+    var meetings = [];
+
+    var table = this.table
+    var params = {
+        TableName:table,
+        Item:{
+            "username": username,
+            "password": password,
+            "info":{
+                "firstname": firstName,
+                "lastname": lastName,
+              //  "company": company,
+                "email": email,
+                "interviewer": isInterviewer,
+                "recruit": isRecruiter,
+                "Meetings": meetings,
+            }
+        }
+    };
+
+    console.log("Adding a new item...");
+    this.docClient.put(params, function(err, data) {
+        if (err) {
+            console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+        } else {
+            console.log("Added item:", JSON.stringify(data, null, 2));
+        }
+    });
+  }
+
+  deleteUser(username){
+    var table = this.table;
+    var params = {
+        TableName:table,
+        Key:{
+            "username": username,
+        }
+      };
+
+
+      console.log("Attempting a conditional delete...");
+      this.docClient.delete(params, function(err, data) {
+          if (err) {
+              console.error("Unable to delete item. Error JSON:", JSON.stringify(err, null, 2));
+              return(0);
+          } else {
+              console.log("DeleteItem succeeded:", JSON.stringify(data, null, 2));
+              return(1);
+          }
+      });
+    }
 
 }
 
