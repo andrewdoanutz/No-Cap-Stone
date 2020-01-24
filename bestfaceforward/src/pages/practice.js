11d5b1e0bcfa0f2c559acb23a35de92b27bdb7b1
Andrew Doan <andrewdoancs@gmail.com>
2020-01-23 18:44:36 -0800

    random questions working

--- bestfaceforward/src/pages/practice.js
+++ bestfaceforward/src/pages/practice.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import {Form, Button, Col, Row} from 'react-bootstrap'
 import Webcam from "react-webcam";
+import Speech from 'speak-tts'
+import questions from '../questions.json'
 
 const videoConstraints = {
     width: 1920,
@@ -8,9 +10,21 @@ const videoConstraints = {
     facingMode: "user"
   };
   
-
 export default class Practice extends Component {
+    randomQuestion(ind){
+        const min = 1;
+        const max = 33;
+        let rand = min + Math.random() * (max - min);
+        while(rand===1){
+            rand = min + Math.random() * (max - min);
+        }
+        rand=Math.round(rand)
+        console.log(rand)
+        console.log(questions[rand])
+        return questions[rand]
+    }
   render() {
+    let question=this.randomQuestion(-1)
     return (
       <div>
           <div className="homeBox">
@@ -21,6 +35,8 @@ export default class Practice extends Component {
                 width={500}
                 videoConstraints={videoConstraints}
                 />
+            <Button onClick={this.randomQuestion}>Next Question</Button>
+            <div>{question}</div>
         </div>
       </div>
     )
