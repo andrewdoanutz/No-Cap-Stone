944e0b0ec1ce99c09d8bfa886b95ffcbc390157e
Bik Nandy <bikramn123@gmail.com>
2020-01-27 22:53:14 -0800

    Revert "Added some css"
    This reverts commit 79e693bd7a7655b3fc87896c7b82d79a9e0717db.

--- bestfaceforward/src/components/navbar.js
+++ bestfaceforward/src/components/navbar.js
@@ -8,7 +8,7 @@ export default class NavBar extends React.Component {
   render() {
     return (
       <div>
-        <Navbar  bg="dark"variant="dark"expand="xl" fixed="top" className="navbar navbar-static-top">
+        <Navbar variant="light" expand="xl" fixed="top" className="navbar">
           <Navbar.Brand href="/login">
             <img
               alt=""
@@ -21,10 +21,10 @@ export default class NavBar extends React.Component {
           </Navbar.Brand>
           <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
             <Nav>
-              <Nav.Item><Nav.Link href="/login" className="link">Login</Nav.Link></Nav.Item>
-              <Nav.Item><Nav.Link href="/videoCall" className="link">Video</Nav.Link></Nav.Item>
-              <Nav.Item><Nav.Link href="/report" className="link">Analysis</Nav.Link></Nav.Item>
-              <Nav.Item><Nav.Link href="/about" className="link">About Us</Nav.Link></Nav.Item>
+              <Nav.Link href="/login" className="link">Login</Nav.Link>
+              <Nav.Link href="/videoCall" className="link">Video</Nav.Link>
+              <Nav.Link href="/report" className="link">Analysis</Nav.Link>
+              <Nav.Link href="/about" className="link">About Us</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Navbar>
