a4a94266ecad823ff3f26344de0c7e3a01e6dd0a
Bik Nandy <bikramn123@gmail.com>
2020-02-03 20:14:10 -0800

    MAde changes to USerDash to include dates

--- bestfaceforward/src/components/UserDash.js
+++ bestfaceforward/src/components/UserDash.js
@@ -3,8 +3,9 @@ import {Container, Col, Row, Button, Card} from 'react-bootstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
 
-var names = [{name: 'Adjon Tahiraj', date: 'Mon, Feb 3', id: 'A576W'}, {name: 'Bik Nandy', date: 'Wed, Feb 5', id: 'A87K2'}, {name: 'Tim Chang', date: 'Wed, Feb 19', id: '4TKLZ'}];
-var namesList = names.map(function(candidate){
+var names = [{name: 'Adjon Tahiraj', date: 'Mon, Feb 3', id: 'A576W'}, {name: 'Bik Nandy', date: 'Wed, Feb 5', id: 'A87K2'}, {name: 'Andrew Doan', date: 'Thurs, Feb 13'
+, id: 'OV3KZ'}, {name: 'Tim Chang', date: 'Wed, Feb 19', id: '4TKLZ'}];
+var candidateList = names.map(function(candidate){
   return <Col> <h3> {candidate.date} </h3>
     <Card  className = "shadow" style={{marginBottom: "10%"}}>
     <Card.Body>
@@ -17,10 +18,7 @@ var namesList = names.map(function(candidate){
           <Button size="lg" variant="primary">Join Meeting</Button>
         </Col>
         <Col>
-          <Button variant = "primary"><FontAwesomeIcon icon={faArrowRight} size='2x'/></Button>
-        </Col>
-        <Col>
-          <Button variant = "info" >Post-interview analysis</Button>
+          <Button variant = "primary"><h5> View Candidate </h5><FontAwesomeIcon icon={faArrowRight} size='2x'/></Button>
         </Col>
 
       </Row>
@@ -35,7 +33,7 @@ export default class Userdash extends Component {
       <div>
         <div className = "scroll">
           <Container>
-            <Row>{ namesList }</Row>
+            <Row>{ candidateList }</Row>
           </Container>
         </div>
       </div>
