38671b6a2c1e0eb623949181d095db7c5b6d72ed
Andrew Doan <andrewdoancs@gmail.com>
2020-02-20 20:59:38 -0800

    basic layout

--- bestfaceforward/src/components/profile.js
+++ bestfaceforward/src/components/profile.js
@@ -1,26 +1,50 @@
 import React from "react";
+import {Card,Col,Row} from 'react-bootstrap';
 
-import "../css/profile.css";
 
 const Profile=(props)=>{
-
-    return (
-      <div className="cont">
-     
-        <img className="image"
-          id="profileImage"
-          src={props.image}
-          alt="profileimage"
-        />
-        
-        <div className="info">
-          <div className="category">Name: </div> <div >{props.name}</div><div/>
-          <div className="category">Year/Major: </div> <div >{props.yearMajor}</div><div/>
-          <div className="category">LinkedIn: </div> <div ><a href={props.linkedIn}>Click Here</a></div><div/>
-        </div>
-      </div>
-    );
-  
+    if(props.right){
+      return (
+        <Card className = "w-50" style={{marginBottom: "10px"}}>
+          <Card.Header>
+            <h1 className="float-right">{props.name}</h1>
+          </Card.Header>
+          <Card.Body>
+            <Card.Text>
+              <Row className="float-right">
+                <Col>
+                  <div>{props.yearMajor}</div>
+                  <div>{props.linkedIn}</div>
+                </Col>
+                <Col>
+                  <img style={{height: "200px"}} src={props.image}/>
+                </Col>
+                
+              </Row>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      );
+    } else {
+      return (
+        <Card className = "w-50" style={{marginBottom: "10px"}}>
+          <Card.Header as="h1">{props.name}</Card.Header>
+          <Card.Body>
+            <Card.Text>
+              <Row>
+                <Col>
+                  <img style={{height: "200px"}} src={props.image}/>
+                </Col>
+                <Col>
+                  <div>{props.yearMajor}</div>
+                  <div>{props.linkedIn}</div>
+                </Col>
+              </Row>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      );
+    }
 }
 
 export default Profile;
